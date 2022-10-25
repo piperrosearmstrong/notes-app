3db2e9d8d3a2db797a20def293b4638f3f78e5b0
Piper-Rose Armstrong <piperrosearmstrong@gmail.com>
2022-10-25 16:37:09 +0100

    Test-driving the NotesView class

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const NotesModel = require('./notesModel');
-
-console.log("The notes app is running.")
+const NotesView = require('./notesView');
 
 const model = new NotesModel();
-console.log(model.getNotes());
+model.addNote('Do a load of laundry.');
+model.addNote('Take my red top to the dry cleaners.');
+model.addNote('Clean off the top of my dresser.');
+
+const view = new NotesView(model);
+
+view.displayNotes();
